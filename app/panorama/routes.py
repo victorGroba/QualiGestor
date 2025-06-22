a7f153e45c7537c74d23d64bc078e2b4432b116d
Marcelo Gomes <marcelogsantos.dev@gmail.com>
2025-06-22 11:40:28 -0300

    inclusão panorama 22/06

--- app/panorama/routes.py
+++ app/panorama/routes.py
@@ -1,9 +1,14 @@
 from flask import Blueprint, render_template
-from ..utils.decorators import login_required
+from flask_login import login_required
 
 panorama_bp = Blueprint('panorama', __name__, template_folder='templates')
 
-@panorama_bp.route('/dashboard')
+@panorama_bp.route('/')
 @login_required
-def dashboard():
-    return render_template('panorama/dashboard.html')
+def index():
+    return render_template('panorama/index.html')
+
+@panorama_bp.route('/filtros')
+@login_required
+def filtros():
+    return render_template('panorama/filtros.html')
